63905d6ee258d04ac4f9ce9fff58a552a4218bee
cfillion <cfillion@users.noreply.github.com>
2015-02-16 01:44:40 -0500

    cheap multi-room support

--- src/chat.cpp
+++ src/chat.cpp
@@ -15,8 +15,9 @@ void Chat::processCommand(const Command &command)
   const Server *server = command.server();
 
   Command broadcast(MSG_COMMAND);
-  broadcast.addArgument(command.peer()->uuid().toString());
   broadcast.addArgument(command.argument(0));
+  broadcast.addArgument(command.peer()->uuid().toString());
+  broadcast.addArgument(command.argument(1));
 
   Q_FOREACH(Peer *peer, server->peers()) {
     peer->send(broadcast);
